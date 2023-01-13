cd09c014c974e8befce94a887804c0baa5f5ba6d
Mihiryadav1 <yadavmihir30@gmail.com>
2023-01-14 00:24:07 +0530

    Message

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Head = () => {
+    return (
+        <>
+            <section className="head">
+                <div className="container flexSB">
+                    <div className="logo">
+                        <h1>ACADEMIA</h1>
+                        <span>ONLINE EDUCATION & LEARNING</span>
+                    </div>
+
+                    <div className="social">
+                        <i className="fa-brands fa-facebook icon"></i>
+                        <i className="fa-brands fa-instagram icon"></i>
+                        <i className="fa-brands fa-twitter icon"></i>
+                        <i className="fa-brands fa-youtube icon"></i>
+                    </div>
+                </div>
+            </section>
+        </>)
+}
+export default Head
---
+++ src/components/common/heading/Headers.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+import Head from "./Head"
+import "./header.css"
+
+const Headers = () => {
+  const [click, setClick] = useState(false);
+  return (
+    <>
+      <Head />
+      <header>
+        <nav className='flexSB'>
+          <ul className={click ? "mobile-nav" : "flexSB"} onClick={() => {
+            setClick(false)
+          }}>
+            <li className="pe-4 list">
+              <a className="linker" href="/">HOME</a>
+            </li>
+            <li className="pe-4 list">
+              <a className="linker" href="/">ABOUT US</a>
+            </li>
+            <li className="pe-4 list">
+              <a className="linker" href="/">TEAM</a>
+            </li>
+            <li className="pe-4 list">
+              <a className="linker" href="/">COURSES</a>
+            </li>
+            <li className="pe-4 list">
+              <a className="linker" href="/">RESOURCES</a>
+            </li>
+            <li className="pe-4 list">
+              <a className="linker" href="/">PRICING</a>
+            </li>
+          </ul>
+
+          <div className="start">
+            <div className="certify">Get Certificate </div>
+          </div>
+          <button className="toggle" onClick={() => { setClick(!click) }}>
+            {click ? <i className="fa-solid fa-bars"></i> : <i class="fa-solid fa-xmark"></i>}
+          </button>
+        </nav>
+      </header>
+    </>
+  )
+}
+export default Headers
